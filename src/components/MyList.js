fe6fb7950081d26c8ccb3b78b2780113a2a54b26
Thandriss <mahonko_yana@mail.ru>
2024-01-06 19:04:52 +0200

    rend

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -4,7 +4,7 @@ const MyList = (props) => {
         <h1>{props.header}</h1>
         <ol>
           {props.items.map((item)=> (
-            <li key={item.id} onClick={()=>props.updateItem(item.id)} style={{ textDecoration: item.clicked?'line-through': 'none'}}>{item.text}</li>
+            <li key={item.id} onClick={()=>props.updateItem(item.id)} style={{ textDecoration: item.clicked?'line-through': ''}}>{item.text}</li>
           ))}
         </ol>
     </div>
