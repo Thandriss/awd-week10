8051579a49119620b8da161b2cde516af941ef90
Thandriss <mahonko_yana@mail.ru>
2024-01-06 18:50:04 +0200

    last task

---
+++ src/components/MyList.js
@@ -0,0 +1,15 @@
+const MyList = (props) => {
+  return (
+    <div>
+        <h1>{props.header}</h1>
+        <ol>
+          {props.items.map((item)=> (
+            <li key={item.id} onClick={()=>props.updateItem(item.id)} style={{ textDecoration: item.clicked?'line-through': 'none'}}>{item.text}</li>
+          ))}
+        </ol>
+    </div>
+  )
+}
+
+
+export default MyList
